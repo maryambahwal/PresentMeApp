a84d7e7c98dd0568a4faf85bb05191cc8454b99d
Maryam Bahwal <mrim2m7md@gmail.com>
2024-09-29 09:26:21 +0300

    Initial Commit

---
+++ MeApp/MeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeAppApp.swift
+//  MeApp
+//
+//  Created by Maryam Bahwal on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct MeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
