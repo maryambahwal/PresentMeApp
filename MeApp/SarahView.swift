a77d7e6d8ab19e240ef369870dd9c53b7dcfe6cc
Sarah Alsurayhi <sr.alsurayhi@gmail.com>
2024-09-29 12:06:28 +0300

    Me App | Sarah

---
+++ MeApp/SarahView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Test
+//
+//  Created by E07 on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            Image("Sarah")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .overlay(Circle() .stroke(Color.orange, lineWidth: 8))
+                         
+            Text("Sarah Alsurayhi")
+                .font(.title)
+            
+            Text("UI/UX Designer and No-Code Web Designer. Got a passion in Hardware and enjoying fixing stuff.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
